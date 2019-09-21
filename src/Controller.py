c49509122cf42104f1ab80127888eff862b2d7f6
Richard Quinlivan <rquinli1@binghamton.edu>
2019-09-21 01:36:50 -0400

    merge plz

--- src/Controller.py
+++ src/Controller.py
@@ -27,7 +27,7 @@ class Controller:
             elif(self.state == "GAMEOVER"):
                 self.gameOver()
 
-    def startLoop1():
+    def startLoop1(self):
         pygame.key.set_repeat(1,50)
         while(self.state == "START1"):
             background = pygame.transform.smoothscale(pygame.image.load("assets/background.jpg").convert_alpha(),(WIDTH, HEIGHT))
@@ -48,19 +48,40 @@ class Controller:
             mouse = pygame.mouse.get_pos()
             click = pygame.mouse.get_pressed() #tuple postition
             if click[0] == 1 and mouse[0] in range(440,590) and mouse[1] in range(400,450):
-                self.state = "GAME"
+                self.state = "START2"
             if click[0] == 1 and mouse[0] in range(50,200) and mouse[1] in range(400,450):
                 sys.exit()
             pygame.display.flip()
 
 
-    def startLoop2():
+    def startLoop2(self):
         pygame.key.set_repeat(1,50)
         while(self.state == "START2"):
-            pass
+            background = pygame.transform.smoothscale(pygame.image.load("assets/background.jpg").convert_alpha(),(WIDTH, HEIGHT))
+            self.screen.blit(background, (0,0))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            font = pygame.font.SysFont("arial", 40, True)
+            title = font.render('Cold War!', True, (250,250,250))
+            self.screen.blit(title, (100,100))
+            pygame.draw.rect(self.screen, (80,208,255), (50,400,150,50)) #pick country
+            pygame.draw.rect(self.screen, (0,192,0), (440,400,150,50)) #select difficulty
+            font = pygame.font.SysFont("arial", 30, True)
+            start_button = font.render('Select Difficulty', True, (0,0,0))
+            self.screen.blit(start_button, (485,415))
+            quit_button = font.render('Pick a Country', True, (0,0,0))
+            self.screen.blit(quit_button, (95,415))
+            mouse = pygame.mouse.get_pos()
+            click = pygame.mouse.get_pressed() #tuple postition
+            if click[0] == 1 and mouse[0] in range(440,590) and mouse[1] in range(400,450):
+                self.state = "GAME"
+            if click[0] == 1 and mouse[0] in range(50,200) and mouse[1] in range(400,450):
+                sys.exit()
+            pygame.display.flip()
 
-    def gameLoop():
+    def gameLoop(self):
         sys.exit()
 
-    def endLoop():
+    def endLoop(self):
         pass
