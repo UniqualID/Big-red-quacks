83f3fe4d4e752f7810dc49faf1bbc14ac6ef7a1d
Richard Quinlivan <rquinli1@binghamton.edu>
2019-09-22 07:58:49 -0400

    last

--- Controller.py
+++ Controller.py
@@ -658,12 +658,13 @@ class Controller:
                 makeNuke(self.player)
 
             ##### END TURN BUTTON #####
-            pygame.draw.rect(self.screen, (88,88,88), (1170, 730, 220, 110)) #x, y, len, wid
+            # pygame.draw.rect(self.screen, (88,88,88), (1170, 730, 220, 110)) #x, y, len, wid
             font2 = pygame.font.Font("assets/fonts/pixelplay.ttf", 45)
             self.screen.blit(font2.render("End Turn", True, (255,255,255)), (1215,755))
             if click[0] == 1 and mouse[0] in range(1170, 1440) and mouse[1] in range(730, 1040):
                 self.turnCounter = self.turnCounter + 1
 
+            self.screen.blit(font2.render(str(self.turnCounter),True, (255,255,255)), (10,0))
             if click[0] == 1 and mouse[0] in range(0,100) and mouse[1] in range(0,100):
                 sys.exit()
             pygame.event.wait()
